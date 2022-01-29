5964a160b31e1bf43c82b70e986997a92085bd8e
Silvi Basith <98403750+Silvi-Basith@users.noreply.github.com>
2022-01-30 00:54:36 +0600

    Add files via upload

---
+++ Task 2/SilviBasith/src/SilviBasith/Main.java
@@ -0,0 +1,23 @@
+package SilviBasith;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student();
+
+        Student student2 = new Student("Silvi Basith");
+
+        Student student3 = new Student("Silvi Basith", 2012020291);
+
+        student1.display();
+    }
+}
+
+/*
+ Name = Silvi Basith
+ ID = 2012020291
+ Section: F
+ mail: cse_2012020291@lus.ac.bd
+ Date: 01-12-2021
+ */
